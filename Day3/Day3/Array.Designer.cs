9c03cde711f42cd397dbb44c9642d0d5ec3699c0
arafataiub <mohdarafat41@gmail.com>
2019-09-15 13:04:17 +0600

    Class_Practice_added

---
+++ Day3/Day3/Array.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Day3
+{
+    partial class Array
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.showbutton1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // showbutton1
+            // 
+            this.showbutton1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.showbutton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.showbutton1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.showbutton1.Location = new System.Drawing.Point(173, 40);
+            this.showbutton1.Name = "showbutton1";
+            this.showbutton1.Size = new System.Drawing.Size(205, 93);
+            this.showbutton1.TabIndex = 0;
+            this.showbutton1.Text = "Show";
+            this.showbutton1.UseVisualStyleBackColor = false;
+            this.showbutton1.Click += new System.EventHandler(this.showbutton1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(182, 169);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(187, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(129, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Reverse Elements";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Array
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 473);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.showbutton1);
+            this.Name = "Array";
+            this.Text = "Array";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button showbutton1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Day3/Day3/Array.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Day3
+{
+    public partial class Array : Form
+    {
+        private int index;
+
+        public Array()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void showbutton1_Click(object sender, EventArgs e)
+
+/*        {
+            int []reverse=new int[] {2,5,7};
+            reverse[0] = 2;
+            reverse[1] = 5;
+            reverse[2] = 7;
+            string message = "The values store into the array are: ";
+            for (index=-1;index<3; index--)
+            {
+                message += +message[index];
+            
+           // string message2 = "The values store into the array in reverse are: " ;
+           MessageBox.Show(message);
+
+
+        }*/
+
+        /* 1 {
+            int []element = new int[] {1, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+            string message ="Elements in Array are: " ;
+
+            for(int i=0; i<10; i++)
+            {
+                message+=+element[i] +" ";
+            }
+
+            MessageBox.Show(message);
+
+            {
+                
+            }*/
+
+            /*            string[]name = new string[10];
+                        name[0] = "Dwaipayan";
+                        name[1] = "Mehedi";
+                        name[2] = "Hasan";
+
+                        int []age =new int[] {12,3,13};
+                        age[0] = 20;
+                        age[1] = 30;
+                        age[2] = 40;
+                        string message = " ";
+                        for (int index = 0; index < 3; index++)
+                        {
+                            message += "Name : " +name[index] + " Age : " +age[index] +"\n";
+                        }
+
+                        MessageBox.Show(message);*/
+
+
+        }
+    }
