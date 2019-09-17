f2c1aa2a67e300dfb9d0233cc847fcbc4632f0a3
BITM LAB <abusayedahmadruet13@gmail.com>
2019-09-17 13:00:58 +0600

    Final_added

--- day4/day4/Form1.Designer.cs
+++ day4/day4/Form1.Designer.cs
@@ -28,13 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.addButton1 = new System.Windows.Forms.Button();
+            this.nameTextBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.showButton1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // addButton1
+            // 
+            this.addButton1.Location = new System.Drawing.Point(318, 81);
+            this.addButton1.Name = "addButton1";
+            this.addButton1.Size = new System.Drawing.Size(75, 23);
+            this.addButton1.TabIndex = 0;
+            this.addButton1.Text = "Add";
+            this.addButton1.UseVisualStyleBackColor = true;
+            this.addButton1.Click += new System.EventHandler(this.addButton1_Click);
+            // 
+            // nameTextBox1
+            // 
+            this.nameTextBox1.Location = new System.Drawing.Point(166, 83);
+            this.nameTextBox1.Name = "nameTextBox1";
+            this.nameTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.nameTextBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mission Script", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(100, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name";
+            // 
+            // showButton1
+            // 
+            this.showButton1.Location = new System.Drawing.Point(318, 164);
+            this.showButton1.Name = "showButton1";
+            this.showButton1.Size = new System.Drawing.Size(75, 23);
+            this.showButton1.TabIndex = 3;
+            this.showButton1.Text = "Show";
+            this.showButton1.UseVisualStyleBackColor = true;
+            this.showButton1.Click += new System.EventHandler(this.showButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(428, 404);
+            this.Controls.Add(this.showButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nameTextBox1);
+            this.Controls.Add(this.addButton1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button addButton1;
+        private System.Windows.Forms.TextBox nameTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button showButton1;
     }
 }
 
--- day4/day4/Form1.cs
+++ day4/day4/Form1.cs
@@ -12,9 +12,40 @@ namespace day4
 {
     public partial class Form1 : Form
     {
+        List<string> names = new List<string> { "Dwaipayan" };
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void addButton1_Click(object sender, EventArgs e)
+        {
+            
+            names.Add(nameTextBox1.Text);
+            ShowCustomer();
+            
+
+            //for(int i=0;i<name.Count();i++)
+            //{
+            //    message += "Name is : " + name[i] + "\n\n";
+            //}
+       
+           
+        }
+        private void ShowCustomer()
+        {
+            string message = " ";
+            foreach (string name in names)
+            {
+                message += "Name is : " + name + "\n\n";
+            }
+            MessageBox.Show(message);
+        }
+
+        private void showButton1_Click(object sender, EventArgs e)
+        {
+            ShowCustomer();
         }
     }
 }
